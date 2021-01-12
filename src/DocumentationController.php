68e1c253459d52c6ac24867e5700707a11bb9193
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2021-01-12 16:11:30 -0500

    WIP

--- src/DocumentationController.php
+++ src/DocumentationController.php
@@ -2,6 +2,7 @@
 
 namespace ArtisanBuild\UI;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\View;
 use League\CommonMark\MarkdownConverterInterface;
@@ -12,22 +13,18 @@ class DocumentationController
 
     public function __invoke(MarkdownConverterInterface $converter, $component, $kit = 'default')
     {
-        $doc  = $this->docPath . '/' . $component . '/' . $kit . '-documentation.md';
+        $componentClass = new (Blade::getClassComponentAliases()[$component]);
+
+        dd($componentClass);
+
         $test = View::exists('artisan-ui::' . $component . '.' . $kit . '-test')
             ? view('artisan-ui::' . $component . '.' . $kit . '-test') : 'Samples Coming Soon';
 
         $testFile = File::exists($this->docPath . '/' . $component . '/' . $kit . '-test.blade.php') ?
             File::get($this->docPath . '/' . $component . '/' . $kit . '-test.blade.php') : 'Test Missing';
 
-        $this->converter = $converter;
-
-        if (!File::exists($doc)) {
-            abort(400);
-        }
-
         return view('artisan-ui::documentation',
             [
-                'documentation' => $this->converter->convertToHtml(File::get($doc)),
                 'testing'       => $test,
                 'testFile'      => $testFile,
             ]);
--- src/View/Components/CodeBlock.php
+++ src/View/Components/CodeBlock.php
@@ -10,6 +10,12 @@ class CodeBlock extends BaseComponent
 
     public $description = 'A code block styled with highlight.js';
 
+    public $expected = [
+        'highlightjs' => '(optional) The language for which you want to use sytnax highlighting (see highlightjs docs)',
+        'pre_*'       => '(wildcard) Any attributes prepended with pre_ will be applied to the pre tag',
+        'code_*'      => '(wildcard) Any attributes prepended with code_ will be applied to the code tag',
+    ];
+
     public function __construct($kit = 'default')
     {
         $this->kit = $kit;
